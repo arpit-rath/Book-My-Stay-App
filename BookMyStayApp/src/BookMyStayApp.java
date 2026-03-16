e47fc6b1f49015205743d4629bf45b86a4a8c3d3
Arpit Rath <arpitrath00@gmail.com>
2026-03-16 14:20:01 +0530

    UC1: Application Entry & Welcome Message

---
+++ BookMyStayApp/src/BookMyStayApp.java
@@ -0,0 +1,13 @@
+public class HotelBookingApp {
+    public static void main(String[] args) {
+
+        // Print welcome message and application details
+        System.out.println("=================================");
+        System.out.println(" Welcome to the Hotel Booking System ");
+        System.out.println(" Application Name: Hotel Booking System");
+        System.out.println(" Version: 1.0");
+        System.out.println("=================================");
+
+        // Program terminates after printing the message
+    }
+}
